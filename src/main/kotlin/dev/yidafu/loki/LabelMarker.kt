ef806d5c66a8de9efd72b9dc8ab0f2e6b24a4673
yidafu <me@yidefu.dev>
2023-08-24 23:24:02 +0800

    feat: LabelMarker impl

--- src/main/kotlin/dev/yidafu/loki/LabelMarker.kt
+++ src/main/kotlin/dev/yidafu/loki/LabelMarker.kt
@@ -1,15 +1,22 @@
 package dev.yidafu.loki
 
 import org.slf4j.Marker
+import java.util.concurrent.CopyOnWriteArrayList
 
-class LabelMarker : Marker {
+class LabelMarker(
+    private val key: String,
+    private val value: String,
+) : Marker {
+
+    private val referenceList = CopyOnWriteArrayList<LabelMarker>()
+
     /**
      * Get the name of this Marker.
      *
      * @return name of marker
      */
     override fun getName(): String {
-        TODO("Not yet implemented")
+        return key
     }
 
     /**
@@ -26,7 +33,17 @@ class LabelMarker : Marker {
      * if 'reference' is null
      */
     override fun add(reference: Marker?) {
-        TODO("Not yet implemented")
+        if (reference == null) {
+            throw IllegalArgumentException("A null value cannot be added to a Marker as reference.")
+        }
+        if (reference !is LabelMarker) {
+            throw IllegalArgumentException("reference must be LabelMarker.")
+        }
+        if (!contains(reference)) {
+            if (!reference.contains(this)) {
+                referenceList.add(reference)
+            }
+        }
     }
 
     /**
@@ -37,11 +54,12 @@ class LabelMarker : Marker {
      * @return true if reference could be found and removed, false otherwise.
      */
     override fun remove(reference: Marker?): Boolean {
-        TODO("Not yet implemented")
+        return referenceList.remove(reference)
     }
 
+    @Deprecated("Deprecated in Java", ReplaceWith("hasReferences()"))
     override fun hasChildren(): Boolean {
-        TODO("Not yet implemented")
+        return hasReferences()
     }
 
     /**
@@ -50,7 +68,7 @@ class LabelMarker : Marker {
      * @return true if this marker has one or more references, false otherwise.
      */
     override fun hasReferences(): Boolean {
-        TODO("Not yet implemented")
+        return referenceList.size > 0
     }
 
     /**
@@ -60,7 +78,7 @@ class LabelMarker : Marker {
      * @return Iterator over the references of this marker
      */
     override fun iterator(): MutableIterator<Marker> {
-        TODO("Not yet implemented")
+        return referenceList.iterator()
     }
 
     /**
@@ -75,7 +93,20 @@ class LabelMarker : Marker {
      * @return Whether this marker contains the other marker.
      */
     override fun contains(other: Marker?): Boolean {
-        TODO("Not yet implemented")
+        if (other == null) {
+            throw IllegalArgumentException("other cannot be null")
+        }
+        if (this == other) {
+            return true
+        }
+        if (hasReferences()) {
+            for (marker in this.iterator()) {
+                if (this == marker) {
+                    return true
+                }
+            }
+        }
+        return false
     }
 
     /**
@@ -87,6 +118,55 @@ class LabelMarker : Marker {
      * @return Whether this marker contains the other marker.
      */
     override fun contains(name: String?): Boolean {
-        TODO("Not yet implemented")
+        if (name == null) {
+            throw IllegalArgumentException("name cannot be null")
+        }
+        if (getName() == name) {
+            return true
+        }
+        if (hasReferences()) {
+            for (marker in this.iterator()) {
+                if (marker.contains(name)) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) return false
+        if (this === other) return true
+        if (other is Marker) {
+            return name == other.name
+        }
+        return false
+    }
+
+    override fun toString(): String {
+        return StringBuilder().apply {
+            append("{ ")
+            append("$name: $value")
+            if (hasReferences()) {
+                append(", ")
+            }
+            val iter = this@LabelMarker.iterator()
+            while (iter.hasNext()) {
+                val label = iter.next()
+                if (label is LabelMarker) {
+                    append("${label.name}: ${label.value}")
+                }
+                if (iter.hasNext()) {
+                    append(", ")
+                }
+            }
+            append(" }")
+        }.toString()
+    }
+
+    override fun hashCode(): Int {
+        var result = key.hashCode()
+        result = 31 * result + value.hashCode()
+        return result
     }
 }
