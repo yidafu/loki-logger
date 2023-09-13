f661287fe33d8961a114442bfd6b55db2263bafd
yidafu <yidafu@hzwaterit.com>
2023-09-13 21:05:01 +0800

    test(loki-core/sender): add case for to JSON String

--- loki-core/src/test/kotlin/dev/yidafu/loki/core/sender/LokiStreamTest.kt
+++ loki-core/src/test/kotlin/dev/yidafu/loki/core/sender/LokiStreamTest.kt
@@ -14,4 +14,16 @@ class LokiStreamTest : FunSpec({
         val str = LokiStream(mapOf("k\"ey" to "value"), arrayOf(arrayOf("1694521198868", "\tlog\nmessage"))).toString()
         str shouldBe "{\"stream\":{\"k\\\"ey\":\"value\"},\"values\":[[\"1694521198868\",\"\\tlog\\nmessage\"]]}"
     }
+
+    test("multiple labels and values") {
+        val str = LokiStream(
+            mapOf("key1" to "value1", "key2" to "value2"),
+            arrayOf(
+                arrayOf("1694521198868", "log message 1"),
+                arrayOf("1694521198868", "log message 2"),
+                arrayOf("1694521198868", "log message 3"),
+            ),
+        ).toString()
+        str shouldBe "{\"stream\":{\"key1\":\"value1\",\"key2\":\"value2\"},\"values\":[[\"1694521198868\",\"log message 1\"],[\"1694521198868\",\"log message 2\"],[\"1694521198868\",\"log message 3\"]]}"
+    }
 })
