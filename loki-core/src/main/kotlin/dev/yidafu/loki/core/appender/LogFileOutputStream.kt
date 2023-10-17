f5431fe92cbd1e378ae90cad6b6c826ea23b17ca
yidafu <yidafu@hzwaterit.com>
2023-10-17 16:09:12 +0800

    refactor(core): LogFileOutputStream remove buffered wrapper

--- loki-core/src/main/kotlin/dev/yidafu/loki/core/appender/LogFileOutputStream.kt
+++ loki-core/src/main/kotlin/dev/yidafu/loki/core/appender/LogFileOutputStream.kt
@@ -1,6 +1,5 @@
 package dev.yidafu.loki.core.appender
 
-import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -8,16 +7,17 @@ import java.io.OutputStream
 import java.nio.channels.FileLock
 
 class LogFileOutputStream(private val file: File) : OutputStream() {
-    private val outputStream: BufferedOutputStream
+//    private val outputStream: BufferedOutputStream
     private val fileOutputStream: FileOutputStream
-    private val BUFFER_SIZE: Int = 8 * 1024
-    private var pos: Int = 0
+//    private val BUFFER_SIZE: Int = 8 * 1024
     init {
         if (!file.parentFile.exists()) {
             file.parentFile.mkdirs()
         }
+
+        // 实时地把日志写入文件里
         fileOutputStream = FileOutputStream(file, true)
-        outputStream = BufferedOutputStream(fileOutputStream, BUFFER_SIZE)
+//        outputStream = BufferedOutputStream(fileOutputStream, BUFFER_SIZE)
     }
 
     override fun write(b: ByteArray) {
@@ -29,46 +29,15 @@ class LogFileOutputStream(private val file: File) : OutputStream() {
         var fileLock: FileLock? = null
         try {
             fileLock = channel.lock()
-            if (buf.size <= BUFFER_SIZE) { // buf 小于 BUFFER_SIZE
-                // 每次 flush 保存缓冲是满的(=BUFFER_SIZE),减少 flush 次数
-                var bufIndex = 0
-                do {
-                    val restOSBufLen = BUFFER_SIZE - pos
-                    val restBuffLen = buf.size - bufIndex
-                    if (restOSBufLen >= restBuffLen) { // OutputStream 剩余 osBuf 能够放下 buf
-                        val bufEndIndex = buf.size
-                        val bufArr = buf.sliceArray(bufIndex..<bufEndIndex)
-                        val bufLen = bufArr.size
-                        write(bufArr, 0, bufLen)
-                        pos += bufLen
-                        bufIndex = bufEndIndex
-                    } else {
-                        val bufEndIndex = bufIndex + restOSBufLen
-                        val bufArr = buf.sliceArray(bufIndex..<bufEndIndex)
-                        val bufLen = bufArr.size
-                        write(bufArr, 0, bufLen)
-                        pos += bufLen
-                        bufIndex = bufEndIndex
-                    }
-
-                    if (pos >= BUFFER_SIZE) {
-                        flush()
-                        pos = 0
-                    }
-                } while (bufIndex < buf.size - 1)
-            } else {
-                // 超过 BUFFER_SIZE 如果走上面缓冲逻辑会触发至少要2次 flush
-                // 直接 write 只需要一次
-                write(buf)
-            }
+            fileOutputStream.write(buf)
         } catch (e: IOException) {
-            println("LogFileOutputStream write byte array failed \n\t${e.stackTraceToString()}")
+            println("LogFileOutputStream write byte array failed\n\t${e.stackTraceToString()}")
         } finally {
             fileLock?.release()
         }
     }
     override fun write(b: ByteArray, off: Int, len: Int) {
-        outputStream.write(b, off, len)
+        fileOutputStream.write(b, off, len)
     }
 
     /**
@@ -88,14 +57,14 @@ class LogFileOutputStream(private val file: File) : OutputStream() {
      * output stream has been closed.
      */
     override fun write(b: Int) {
-        outputStream.write(b)
+        fileOutputStream.write(b)
     }
 
     override fun close() {
-        outputStream.close()
+        fileOutputStream.close()
     }
 
     override fun flush() {
-        outputStream.flush()
+        fileOutputStream.flush()
     }
 }
